addfa5b8eb81010b6adbe273212c283f9cff8293
BiyCck <98288128+BiyCck@users.noreply.github.com>
2023-09-20 13:00:48 +0200

    Fifth Lesson - Added Create for API

--- src/main/java/com/cicek/customer/CustomerListDataAccessService.java
+++ src/main/java/com/cicek/customer/CustomerListDataAccessService.java
@@ -41,5 +41,15 @@ public class CustomerListDataAccessService implements CustomerDao{
 
     }
 
+    @Override
+    public void insertCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    @Override
+    public boolean existsPersonWithEmail(String email) {
+        return customers.stream().anyMatch(customer -> customer.getEmail().equals(email));
+    }
+
 
 }
--- src/main/java/com/cicek/customer/CustomerService.java
+++ src/main/java/com/cicek/customer/CustomerService.java
@@ -1,5 +1,6 @@
 package com.cicek.customer;
 
+import com.cicek.exception.DuplicateResourceException;
 import com.cicek.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -22,6 +23,24 @@ public class CustomerService {
     public Customer getCustomer(Integer id){
         return customerDao.selectCustomerById(id).orElseThrow(
                 () -> new ResourceNotFoundException("customer with id [%s] not found".formatted(id))
+        );
+    }
+
+    public void addCustomer(
+        CustomerRegistrationRequest customerRegistrationRequest
+    ) {
+        if (customerDao.existsPersonWithEmail(customerRegistrationRequest.email())){
+            throw new DuplicateResourceException(
+                    "email already taken"
+            );
+        }
+
+        customerDao.insertCustomer(
+                new Customer(
+                        customerRegistrationRequest.name(),
+                        customerRegistrationRequest.email(),
+                        customerRegistrationRequest.age()
+                )
         );
     }
 }
